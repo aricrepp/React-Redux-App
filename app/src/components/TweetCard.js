48fe41495446fdacc40b4e720445df742a964d2c
Aric Repp <aricmrepp@gmail.com>
2020-06-23 23:00:54 -0400

    Tweaking MVP

--- app/src/components/TweetCard.js
+++ app/src/components/TweetCard.js
@@ -2,13 +2,18 @@ import React, {useState, useEffect} from 'react';
 import check from '../assets/check.png';
 import loading from '../assets/poop.gif';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import {connect} from 'react-redux';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import ReplayIcon from '@material-ui/icons/Replay';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
 import {getQuote} from '../actions/actions';
 
 
@@ -46,6 +51,12 @@ const useStyles = makeStyles((theme) => ({
     },
     quoteCon: {
         padding: '20px',
+    },
+    icons: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignContent: 'center',
     }
     
   }));
@@ -88,6 +99,22 @@ const TweetCard = ({getQuote, quote, isFetching, error }) => {
                 {quote}
                 </Typography>
             </CardContent>
+            <CardActions className={classes.icons}>
+
+        <IconButton aria-label="add to favorites">
+          <ChatBubbleOutlineIcon />
+        </IconButton>
+        <IconButton aria-label="replay">
+          <ReplayIcon />
+        </IconButton>
+        <IconButton aria-label="heart">
+          <FavoriteBorderIcon />
+        </IconButton>
+        <IconButton aria-label="retweet">
+          <PublishIcon />
+        </IconButton>
+
+      </CardActions>
         </Card>
         <button onClick={getQuote}>Get Quote!</button>
         </>
